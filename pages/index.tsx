fca1757d9c2da35ccad914ab2bdd777e32e38550
michael.bowen <michael.bowen@mymail.champlain.edu>
2022-11-03 12:42:19 -0400

    Removed compression from unity build

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
+
 export default function Home() {
-  return (
-    <iframe width={'100%'} height={'1000'} src={'/beta-app/WebGL/index.html'}/>
+  return (<>
+    
+        <iframe width={'100%'} height={'1000'} src={'/beta-app/WebGL/index.html'}/>
+   
+      </>
+
   )
 }
