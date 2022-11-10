2ee5689080efef28706c8e1a2679c8dedfd1db35
michael.bowen <michael.bowen@mymail.champlain.edu>
2022-11-10 11:39:20 -0500

    Updated iframes for beta apps

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (<>
     
-        <iframe width={'100%'} height={'1000'} src={'/beta-app/WebGL/index.html'}/>
+        <iframe width={'100%'} height={'1000'} src={'/beta-app/WebGL/prototype-1/index.html'}/>
+        <iframe width={'100%'} height={'1000'} src={'/beta-app/WebGL/prototype-2/index.html'}/>
    
       </>
 
