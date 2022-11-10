3984dae12e09f445669c29ff39c13a72fbfc5730
michael.bowen <michael.bowen@mymail.champlain.edu>
2022-11-10 12:08:54 -0500

    fixed build path

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-
-
 export default function Home() {
   return (<>
     
-        <iframe width={'100%'} height={'1000'} src={'/beta-app/WebGL/prototype-1/index.html'}/>
-        <iframe width={'100%'} height={'1000'} src={'/beta-app/WebGL/prototype-2/index.html'}/>
+        <iframe width={'100%'} height={'1000'} src={'/beta-app/prototype-1/WebGL/index.html'}/>
+        <iframe width={'100%'} height={'1000'} src={'/beta-app/prototype-2/WebGL/index.html'}/>
    
       </>
 
